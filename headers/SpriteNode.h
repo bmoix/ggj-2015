d2985b46eab847fbdedb3f20ac6efdb1d469fcfc
Bernat Moix <bernatmoix@gmail.com>
2015-01-24 01:22:31 +0100

    setPosition sprite

--- headers/SpriteNode.h
+++ headers/SpriteNode.h
@@ -8,8 +8,9 @@ class SpriteNode: public SceneNode {
     public:
         explicit SpriteNode(const sf::Texture& texture);
         SpriteNode(const sf::Texture& texture, const sf::IntRect& textureRect);
-
         virtual void setSize(sf::Vector2u desiredSize);
+        void setPosition(const sf::Vector2f &position);
+        void setSize(sf::Vector2u desiredSize); 
         void setColor(sf::Color color);
         sf::Color getColor();
 
